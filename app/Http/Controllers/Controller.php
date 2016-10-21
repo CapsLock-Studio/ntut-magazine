98f294be6e7527ee888752de6b210bb4d2037582
Calvin <calvin.peak@capslock.tw>
2016-10-21 19:09:14 +0900

    Add check flow to base controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Route as RouteModel;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +13,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function __destruct()
+    {
+        $route = Route::currentRouteName();
+        $name  = explode(".", $route);
+        $name  = array_shift($name);
+        $title = RouteModel::where("route", $route)->first();
+        view()->share("controller", $name);
+        view()->share($name, "active");
+        if ($title && Auth::check() && !request()->wantsJson()) {
+            view()->share("title", $title->name);
+            echo view()->make($route);
+        }
+    }
 }
