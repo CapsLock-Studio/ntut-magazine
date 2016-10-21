43a4e1c19514c91fcdd9341e08e3eee26221a801
Calvin <calvin.peak@capslock.tw>
2016-10-21 19:33:52 +0900

    Remove translate mapping by db model, use larval default translate function instead.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Auth;
-use App\Route as RouteModel;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
@@ -24,11 +23,10 @@ class Controller extends BaseController
         $route = Route::currentRouteName();
         $name  = explode(".", $route);
         $name  = array_shift($name);
-        $title = RouteModel::where("route", $route)->first();
         view()->share("controller", $name);
         view()->share($name, "active");
         if ($title && Auth::check() && !request()->wantsJson()) {
-            view()->share("title", $title->name);
+            view()->share("title", $name);
             echo view()->make($route);
         }
     }
