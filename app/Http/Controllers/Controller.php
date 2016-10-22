17fff393dfe5853e3f972f2626bc8c0b6543cd00
Calvin <calvin.peak@capslock.tw>
2016-10-22 18:39:38 +0900

    Remove middleware in constructor.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
@@ -15,19 +14,11 @@ class Controller extends BaseController
 
     public function __construct()
     {
-        $this->middleware('auth');
+        
     }
 
     public function __destruct()
     {
-        $route = Route::currentRouteName();
-        $name  = explode(".", $route);
-        $name  = array_shift($name);
-        view()->share("controller", $name);
-        view()->share($name, "active");
-        if ($title && Auth::check() && !request()->wantsJson()) {
-            view()->share("title", $name);
-            echo view()->make($route);
-        }
+        
     }
 }
