6e891251870fa3b321af25de728ec2d86b78893a
Calvin <calvin.peak@capslock.tw>
2016-10-24 00:15:43 +0900

    Add javascript for sorting carousel.

---
+++ public/dist/js/pages/carousels.js
@@ -0,0 +1,14 @@
+(function($) {
+    var sortable = $('#sortable');
+    if (sortable.length > 0) {
+        sortable.sortable({
+            placeholder: 'ui-state-highlight'
+        });
+
+        sortable.on('sortupdate', function(event, ui){
+            $('.sort-item').each(function(index){
+                $(this).val(index + 1);
+            });
+        });
+    }
+})(jQuery);
