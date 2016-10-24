44bb60ddfc7c922f772da392bb5777cb17a867c1
Calvin <calvin.peak@capslock.tw>
2016-10-24 13:05:40 +0900

    Complete Delete action.

--- app/Http/Controllers/Admin/CarouselsController.php
+++ app/Http/Controllers/Admin/CarouselsController.php
@@ -11,6 +11,21 @@ class CarouselsController extends Controller
 
     public function index(Request $request)
     {
+        view()->share('flashMessage', $request->session()->get('flashMessage'));
+        view()->share('flashStatus', $request->session()->get('flashStatus'));
+        view()->share('carousels', Carousel::all());
+    }
 
+    public function destroy(Request $request, $id)
+    {
+        if (Carousel::destroy($id)) {
+            $request->session()->flash('flashMessage', "編號 {$id} 刪除成功。");
+            $request->session()->flash('flashStatus', 'success');
+        } else {
+            $request->session()->flash('flashMessage', '刪除失敗，請洽系統管理員協詢處理。');
+            $request->session()->flash('flashStatus', 'warning');
+        }
+
+        return redirect()->action('Admin\CarouselsController@index');
     }
 }
