acfdc01fefe701ac4aac97987933d050f449da99
Calvin <calvin.peak@capslock.tw>
2016-10-25 13:24:10 +0900

    Add blank state to index.

--- resources/views/admin/carousels/index.blade.php
+++ resources/views/admin/carousels/index.blade.php
@@ -27,47 +27,53 @@
           <div class="box-content box-no-padding">
             <div class="responsive-table">
               <div class="scrollable-table sortable-container">
-                <table class="table" style="margin-bottom:0;">
-                  <thead>
-                  <tr>
-                    <th>圖片</th>
-                    <th>文字</th>
-                    <th>連結</th>
-                    <th></th>
-                  </tr>
-                  </thead>
-                  <tbody id="sortable" data-sortable-axis="y" data-sortable-connect=".sortable">
-                  @foreach ($carousels as $carousel)
+                @if ($carousels->count() > 0)
+                  <table class="table" style="margin-bottom:0;">
+                    <thead>
                     <tr>
-                      <input type="hidden" class="sort-item" name="carousels[{{ $carousel->id }}][order]" value="{{ $carousel->order }}" />
-                      <input type="hidden" class="sort-item" name="carousels[{{ $carousel->id }}][id]" value="{{ $carousel->id }}" />
-                      <td>
-                        <div class="preview">
-                          <img src="{{ $carousel->image->url('thumb') }}" />
-                        </div>
-                      </td>
-                      <td>
-                        {{ $carousel->title }}
-                      </td>
-                      <td>
-                        <a class="btn btn-default" href="{{ $carousel->url }}" target="_blank">
-                          <i class="fa fa-link"></i>
-                        </a>
-                      </td>
-                      <td>
-                        <div class='text-right'>
-                          <a class="btn btn-default" href="/admin/carousels/{{ $carousel->id }}">
-                            <i class='fa fa-pencil'></i>
-                          </a>
-                          <a href="/admin/carousels/{{ $carousel->id }}" class="btn btn-danger" method="DELETE" data-token="{{ csrf_token() }}" data-confirm="你確定要刪除嗎？提醒您，刪除後的資料無法回覆。">
-                            <i class='fa fa-remove'></i>
-                          </a>
-                        </div>
-                      </td>
+                      <th>圖片</th>
+                      <th>文字</th>
+                      <th>連結</th>
+                      <th></th>
                     </tr>
-                  @endforeach
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody id="sortable" data-sortable-axis="y" data-sortable-connect=".sortable">
+                    @foreach ($carousels as $carousel)
+                      <tr>
+                        <input type="hidden" class="sort-item" name="carousels[{{ $carousel->id }}][order]" value="{{ $carousel->order }}" />
+                        <input type="hidden" class="sort-item" name="carousels[{{ $carousel->id }}][id]" value="{{ $carousel->id }}" />
+                        <td>
+                          <div class="preview">
+                            <img src="{{ $carousel->image->url('thumb') }}" />
+                          </div>
+                        </td>
+                        <td>
+                          {{ $carousel->title }}
+                        </td>
+                        <td>
+                          <a class="btn btn-default" href="{{ $carousel->url }}" target="_blank">
+                            <i class="fa fa-link"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <div class='text-right'>
+                            <a class="btn btn-default" href="/admin/carousels/{{ $carousel->id }}/edit">
+                              <i class='fa fa-pencil'></i>
+                            </a>
+                            <a href="/admin/carousels/{{ $carousel->id }}" class="btn btn-danger" method="DELETE" data-token="{{ csrf_token() }}" data-confirm="你確定要刪除嗎？提醒您，刪除後的資料無法回覆。">
+                              <i class='fa fa-remove'></i>
+                            </a>
+                          </div>
+                        </td>
+                      </tr>
+                    @endforeach
+                    </tbody>
+                  </table>
+                @else
+                  <div style="padding: 10px 20px; text-align: center;">
+                    <div class="text-muted">目前沒有設定輪播，馬上去 <a href="/admin/carousels/create">新增</a></div>
+                  </div>
+                @endif
               </div>
             </div>
           </div>
