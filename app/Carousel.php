134500333138ce115f3208891f10e3d6f82b89d5
Calvin <calvin.peak@capslock.tw>
2016-10-25 11:59:35 +0900

    Set carousel image in three styles: thumb, medium, large.

--- app/Carousel.php
+++ app/Carousel.php
@@ -16,9 +16,11 @@ class Carousel extends Model implements StaplerableInterface {
     public function __construct(array $attributes = array()) {
         $this->hasAttachedFile('image', [
             'styles' => [
-                'medium' => '300x300',
-                'thumb' => '100x100'
-            ]
+                'large' => '1920x800',
+                'medium' => '960x400',
+                'thumb' => '480x200',
+            ],
+            'default_url' => 'https://placeholdit.imgix.net/~text?txtsize=33&txt=1920%C3%97800&w=960&h=400',
         ]);
 
         parent::__construct($attributes);
