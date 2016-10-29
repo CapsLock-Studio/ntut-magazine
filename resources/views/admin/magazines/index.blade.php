63d363b6cbfe43bbc36630772dc0eb40eaf0cd8b
Calvin <calvin.peak@capslock.tw>
2016-10-30 04:37:44 +0900

    Use data table to layout.

--- resources/views/admin/magazines/index.blade.php
+++ resources/views/admin/magazines/index.blade.php
@@ -17,48 +17,44 @@
             <a class="btn box-collapse btn-xs btn-link" href="#"><i></i></a>
           </div>
         </div>
-        <div class="box-content box-no-padding">
+        <div class="box-body">
           <div class="responsive-table">
             <div class="scrollable-table sortable-container">
-              @if ($magazines->count() > 0)
-                <table class="table" style="margin-bottom:0;">
-                  <thead>
+              <table class="table table-bordered table-striped data-table" style="margin-bottom:0;">
+                <thead>
+                <tr>
+                  <th>編號</th>
+                  <th>圖片</th>
+                  <th>文字</th>
+                  <th></th>
+                </tr>
+                </thead>
+                <tbody id="sortable" data-sortable-axis="y" data-sortable-connect=".sortable">
+                @foreach ($magazines as $magazine)
                   <tr>
-                    <th>圖片</th>
-                    <th>文字</th>
-                    <th></th>
+                    <td>{{ $magazine->id }}</td>
+                    <td>
+                      <div class="preview">
+                        <img src="{{ $magazine->image->url('thumb') }}" style="width: 200px;" />
+                      </div>
+                    </td>
+                    <td>
+                      {{ $magazine->title }}
+                    </td>
+                    <td>
+                      <div class='text-right'>
+                        <a class="btn btn-default" href="/admin/magazines/{{ $magazine->id }}/edit">
+                          <i class='fa fa-pencil'></i>
+                        </a>
+                        <a href="/admin/magazines/{{ $magazine->id }}" class="btn btn-danger" method="DELETE" data-token="{{ csrf_token() }}" data-confirm="你確定要刪除嗎？提醒您，刪除後的資料無法回覆。">
+                          <i class='fa fa-remove'></i>
+                        </a>
+                      </div>
+                    </td>
                   </tr>
-                  </thead>
-                  <tbody id="sortable" data-sortable-axis="y" data-sortable-connect=".sortable">
-                  @foreach ($magazines as $magazine)
-                    <tr>
-                      <td>
-                        <div class="preview">
-                          <img src="{{ $magazine->image->url('thumb') }}" style="width: 200px;" />
-                        </div>
-                      </td>
-                      <td>
-                        {{ $magazine->title }}
-                      </td>
-                      <td>
-                        <div class='text-right'>
-                          <a class="btn btn-default" href="/admin/magazines/{{ $magazine->id }}/edit">
-                            <i class='fa fa-pencil'></i>
-                          </a>
-                          <a href="/admin/magazines/{{ $magazine->id }}" class="btn btn-danger" method="DELETE" data-token="{{ csrf_token() }}" data-confirm="你確定要刪除嗎？提醒您，刪除後的資料無法回覆。">
-                            <i class='fa fa-remove'></i>
-                          </a>
-                        </div>
-                      </td>
-                    </tr>
-                  @endforeach
-                  </tbody>
-                </table>
-              @else
-                <div style="padding: 10px 20px; text-align: center;">
-                  <div class="text-muted">目前沒有期刊，馬上去 <a href="/admin/magazines/create">新增</a></div>
-                </div>
-              @endif
+                @endforeach
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
