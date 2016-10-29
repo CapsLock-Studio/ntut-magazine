a8e145e96bb347183fb6a2a01d4e2c4c4ff311d8
Calvin <calvin.peak@capslock.tw>
2016-10-29 22:57:14 +0900

    Add magazine model and migration.

---
+++ app/Magazine.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Codesleeve\Stapler\ORM\StaplerableInterface;
+use Codesleeve\Stapler\ORM\EloquentTrait;
+
+class Magazine extends Model implements StaplerableInterface {
+{
+    use EloquentTrait;
+
+    protected $fillable = ['image', 'title'];
+
+    public function __construct(array $attributes = array()) {
+        $this->hasAttachedFile('image', [
+            'styles' => [
+                'large' => '1920',
+                'medium' => '960',
+                'thumb' => '480',
+            ],
+            'default_url' => 'https://placeholdit.imgix.net/~text?txtsize=33&txt=Image&w=400&h=400'
+        ]);
+
+        parent::__construct($attributes);
+    }
+}
