87158c35bc1d98614e56244085a0197017cb29ec
Calvin <calvin.peak@capslock.tw>
2016-10-31 13:26:51 +0900

    Add empty video controller.

---
+++ app/Http/Controllers/Admin/VideosController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+class VideosController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function create(Request $request)
+    {
+
+    }
+
+    public function edit(Request $request, $id)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        
+    }
+}
