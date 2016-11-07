b698f3afc7541744c2693b1a9acddbdf43cc784a
Calvin Huang <calvin.peak@capslock.tw>
2016-11-07 15:52:31 +0900

    Add unify theme required js and css.

---
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -0,0 +1,7 @@
+<div class="breadcrumbs-v3 img-v1">
+  <div class="container text-center">
+    <p>Portfolio</p>
+    <h1>Portfolio 2 Columns Grid No Space</h1>
+  </div>
+  <!--/end container-->
+</div>
