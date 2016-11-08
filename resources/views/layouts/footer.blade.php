c4788df5d6fcb95e20ab6c6058c5cfc75e78c6b9
Calvin Huang <calvin.peak@capslock.tw>
2016-11-08 18:42:29 +0900

    Complete footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,116 +2,28 @@
   <div class="footer">
     <div class="container">
       <div class="row">
-        <!-- About -->
-        <div class="col-md-3 md-margin-bottom-40">
-          <a href="index.html"><img id="logo-footer" class="footer-logo" src="dist/img/logo2-default.png" alt=""></a>
-          <p>About Unify dolor sit amet, consectetur adipiscing elit. Maecenas eget nisl id libero tincidunt sodales.</p>
-          <p>Duis eleifend fermentum ante ut aliquam. Cras mi risus, dignissim sed adipiscing ut, placerat non arcu.</p>
-        </div><!--/col-md-3-->
-        <!-- End About -->
-
-        <!-- Latest -->
-        <div class="col-md-3 md-margin-bottom-40">
-          <div class="posts">
-            <div class="headline"><h2>Latest Posts</h2></div>
-            <ul class="list-unstyled latest-list">
-              <li>
-                <a href="#">Incredible content</a>
-                <small>May 8, 2014</small>
-              </li>
-              <li>
-                <a href="#">Best shoots</a>
-                <small>June 23, 2014</small>
-              </li>
-              <li>
-                <a href="#">New Terms and Conditions</a>
-                <small>September 15, 2014</small>
-              </li>
-            </ul>
-          </div>
-        </div><!--/col-md-3-->
-        <!-- End Latest -->
-
-        <!-- Link List -->
-        <div class="col-md-3 md-margin-bottom-40">
-          <div class="headline"><h2>Useful Links</h2></div>
-          <ul class="list-unstyled link-list">
-            <li><a href="#">About us</a><i class="fa fa-angle-right"></i></li>
-            <li><a href="#">Portfolio</a><i class="fa fa-angle-right"></i></li>
-            <li><a href="#">Latest jobs</a><i class="fa fa-angle-right"></i></li>
-            <li><a href="#">Community</a><i class="fa fa-angle-right"></i></li>
-            <li><a href="#">Contact us</a><i class="fa fa-angle-right"></i></li>
-          </ul>
-        </div><!--/col-md-3-->
-        <!-- End Link List -->
-
-        <!-- Address -->
-        <div class="col-md-3 map-img md-margin-bottom-40">
-          <div class="headline"><h2>Contact Us</h2></div>
+        <div class="col-xs-12 text-center">
+          <p>
+            國立臺北科技大學，所刊載內容均受著作權法保護
+            <span style="margin-left: 20px;">
+              <a target="_blank" href="https://www.ntut.edu.tw/files/13-1021-20385.php">隱私權政策宣告</a> | <a target="_blank" href="http://cnc.ntut.edu.tw/ezfiles/8/1008/img/1233/CCSG-001.pdf">資訊安全政策宣告</a>
+            </span>
+          </p>
           <address class="md-margin-bottom-40">
-            25, Lorem Lis Street, Orange <br />
-            California, US <br />
-            Phone: 800 123 3456 <br />
-            Fax: 800 123 3456 <br />
-            Email: <a href="mailto:info@anybiz.com" class="">info@anybiz.com</a>
+            10608 台北市忠孝東路三段一號  Tel: (886-2) 2771-2171
           </address>
-        </div><!--/col-md-3-->
-        <!-- End Address -->
+        </div>
       </div>
     </div>
   </div><!--/footer-->
-
   <div class="copyright">
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <p>
-            2015 &copy; All Rights Reserved.
-            <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+          <p style="color: #757575; font-size: 12px; margin: 10px 0;">
+            Copyright © 2016 <a target="_blank" href="https://capslock.tw" style="color: #068894;">CapsLock Studio</a>. All rights reserved.
           </p>
         </div>
-
-        <!-- Social Links -->
-        <div class="col-md-6">
-          <ul class="footer-socials list-inline">
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook">
-                <i class="fa fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Skype">
-                <i class="fa fa-skype"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus">
-                <i class="fa fa-google-plus"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Linkedin">
-                <i class="fa fa-linkedin"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest">
-                <i class="fa fa-pinterest"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter">
-                <i class="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Dribbble">
-                <i class="fa fa-dribbble"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <!-- End Social Links -->
       </div>
     </div>
   </div><!--/copyright-->
