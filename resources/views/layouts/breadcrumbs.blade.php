2a316e8c4386427c29d42d9c734d00ec926dc10f
Calvin Huang <calvin.peak@capslock.tw>
2016-11-08 17:44:39 +0900

    Remove breadcrumb blade template because this part already integrate to content.

--- resources/views/layouts/breadcrumbs.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="breadcrumbs-v3 img-v1">
-  <div class="container text-center">
-    <p>Portfolio</p>
-    <h1>Portfolio 2 Columns Grid No Space</h1>
-  </div>
-  <!--/end container-->
-</div>
